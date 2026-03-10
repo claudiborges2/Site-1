d73758699b7b8796c12e6ce821b6a25fcf90a42e
claudiborges2 <claudiborges2@gmail.com>
2026-03-09 21:34:31 -0300

    Ajuste e estudo dev

--- Javascript testes.js
+++ Javascript testes.js
@@ -5,12 +5,70 @@ console.log("Nota inserida:, nota")
 
     if (nota >=7)
     {
-        document.getElementById("resultado").innerHTML = "Aluno aprovado"
+        document.getElementById("aprovado").innerHTML = "Aluno aprovado"
         alert("Aluno aprovado")
     }
     else{
-        document.getElementById("resultado").innerHTML = "Aluno reprovado"
-        alert("Aluno reprovado")       
+        document.getElementById("aprovado").innerHTML = "Aluno super reprovado"
+        alert("Aluno super reprovado")       
     }
     
+}
+
+function trocaCor()
+{
+    const div1 = document.getElementById("div-1");
+    const div2 = document.getElementById("div-2");
+    const div3 = document.getElementById("div-3");
+
+    div1.style.backgroundColor = "red";
+    div2.style.backgroundColor = "green";
+    div3.style.backgroundColor = "blue";
+}
+
+function retornaCor()
+{
+    const div1 = document.getElementById("div-1");
+    const div2 = document.getElementById("div-2");
+    const div3 = document.getElementById("div-3");
+
+    div1.style.backgroundColor = " ";
+    div2.style.backgroundColor = " ";
+    div3.style.backgroundColor = " ";
+
+}
+
+function efetuaOperação()
+{
+     let num1 = parseFloat(document.getElementById("num1").value);
+     let num2 = parseFloat(document.getElementById("num2").value);
+     let operador = document.getElementById("operador").value;
+
+    switch (operador)
+    {
+        
+        case "+": 
+            resultado = num1 + num2 
+            break;
+        case "-":
+            resultado = num1 - num2 
+            break; 
+        case "*": 
+            resultado = num1 * num2 
+            break; 
+        case "/": 
+            if(num2 == 0)
+            {
+                resultado = "ivisão por 0 é invelida!"
+            }
+            resultado = num1 / num2 
+            break; 
+
+        default: 
+            resultado =  "operador inválido!"
+        
+    }
+{   document.getElementById("resultado").innerHTML = "Resultado: " + resultado;}
+
+
 }
