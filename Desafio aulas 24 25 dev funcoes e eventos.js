a4cfedb26fcfbb8070a4f15cea03c9e55cef0276
claudiborges2 <claudiborges2@gmail.com>
2026-03-13 14:33:01 -0300

    Aulas back-end and Fron-end

---
+++ Desafio aulas 24 25 dev funcoes e eventos.js
@@ -0,0 +1,12 @@
+function numeroPedido(text)
+{
+    document.body.style.backgroundColor = Text;
+}
+    Document.getElementById('1Button').addEventListener('click', function(){numeroPedido('1');});
+    Document.getElementById('2Button').addEventListener('click',function(){numeroPedido('2');});
+    Document.getElementById('3Button').addEventListener('click', function(){numeroPedido('3');});
+    Document.getElementById('4Button').addEventListener('click', function(){numeroPedido('4');});
+
+      Document.getElementById('inputBox').addEventListener('keypress' , function (event){
+        alert("Tecla precionada:" + event.key)  
+            });
